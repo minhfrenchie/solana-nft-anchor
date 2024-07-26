d09c95ea5bd5e583140d8a3f8cc5cda9dd9c158d
minhfrenchie <KalynZubek514@gmail.com>
2024-07-26 09:44:26 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("6Ka8tuxtKhDXMXwh9HvrbxzHMYWiX4takupDFPgkX6zA");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
